6664ecedf72ca663f1f8af4337b29104a6912274
Derick <derickchoskinson@gmail.com>
2022-03-02 22:22:56 -0600

    load draft functionality

--- frontend/src/pages/Drafts.tsx
+++ frontend/src/pages/Drafts.tsx
@@ -3,11 +3,21 @@ import { useContext } from "react";
 import { AuthContext } from "../context/auth";
 import { FETCH_DRAFTS_QUERY } from "../utils/graphql/graphql";
 
-export default function Drafts() {
-    const { loading, error, data } = useQuery(FETCH_DRAFTS_QUERY)
+export default function Drafts(): any {
+    const { loading, data: posts, error } = useQuery(FETCH_DRAFTS_QUERY)
     const { user } = useContext(AuthContext);
-    console.log(data)
-    return <>
-    </>
+    console.log(posts, user)
+
+    if (loading) return <p>Loading ...</p>;
+    if (error) return `Error! ${error}`;
+
+    return (<div>Post drafts</div>)
+
+
+
+
+
+
+
 }
 
